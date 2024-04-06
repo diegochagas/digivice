189009dd14ed5f3a7416ef224ebe19f34fea009f
Diego Chagas <drochachagas@gmail.com>
2024-04-06 02:01:48 -0300

    add video assets

---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+const digiviceEvolution = document.getElementById('btn-evolution')
+const digivicePrev = document.getElementById('btn-prev')
+const digiviceNext = document.getElementById('btn-next')
+
+digiviceEvolution.addEventListener('click', () => {
+  console.log('evolution')
+})
+
+digivicePrev.addEventListener('click', () => {
+  console.log('previous digimon')
+})
+
+digiviceNext.addEventListener('click', () => {
+  console.log('next digimon')
+})
