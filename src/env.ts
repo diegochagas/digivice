f3b30c4281d74e3527579b9783cb825a94ea0af0
Diego Chagas <drochachagas@gmail.com>
2024-05-02 08:51:33 -0300

    fix env variables

--- src/env.ts
+++ src/env.ts
@@ -5,11 +5,7 @@ export const env = createEnv({
   server: {
     APP_URL: z.string().url(),
   },
-  client: {
-    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
-  },
   runtimeEnv: {
     APP_URL: process.env.APP_URL,
-    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
 })
