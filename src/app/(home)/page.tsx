344db48df9bb7474e10fe526eb217f22a81e2a1c
Diego Chagas <drochachagas@gmail.com>
2024-05-03 22:04:32 -0300

    replace api by simple json

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,36 +1,18 @@
+import Image from 'next/image'
 import { Crests } from '@/data/types/crest'
-import { api } from '@/data/api'
+import crestsJson from '@/data/crests.json'
 import { Digivice } from '../components/digivice'
 import { Crest } from '../components/crest'
-import Image from 'next/image'
 
-async function getCrests(): Promise<Crests> {
-  try {
-    const response = await api('/crests', {
-      next: {
-        revalidate: 60 * 60, // 1 hour
-      },
-    })
-
-    const crests = await response.json()
-  
-    return crests
-  } catch(err) {
-    console.error('ERROR: ', err)
-
-    return []
-  }
-}
-
-export default async function Home() {
-  const crests: Crests = await getCrests()
+export default function Home() {
+  const { crests } = crestsJson
 
   return (
     <main className="flex justify-around items-center h-screen relative">
       <div className="flex justify-around items-center flex-wrap w-full max-w-[1024px] my-0 mx-auto">
-        <Digivice crests={crests} />
+        <Digivice crests={crests as Crests} />
 
-        <Crest crests={crests} />
+        <Crest crests={crests as Crests} />
       </div>
 
       <Image
