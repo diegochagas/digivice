bb5a5d8a2e4e8475c4d852fae2730e28232b804c
Diego Chagas <drochachagas@gmail.com>
2024-05-07 23:50:01 -0300

    test

--- src/app/components/assets.tsx
+++ src/app/components/assets.tsx
@@ -22,6 +22,7 @@ export function Assets({
   const videoRef = useRef<HTMLVideoElement>(null)
   const [videoSrc, setVideoSrc] = useState('/videos/courage/agumon.mp4')
   const [imageSrc, setImageSrc] = useState('/images/courage/koromon.png')
+  const [videoStarted, setVideoStarted] = useState(false);
     
   useEffect(() => {
     const isUltimateForm = digimonIndex === 5
@@ -70,26 +71,20 @@ export function Assets({
         />
       )}
 
-      {isVideoPlaying && (
+      {/* {!isVideoPlaying && videoStarted && (
         <div className="text-white box-border inline-block absolute w-20 h-20 z-50 top-[86px] left-[99px] -rotate-90">
           <div className="box-border inline-block absolute left-2 w-4 bg-current animate-loading animation-delay-[-0.24s]" />
           <div className="box-border inline-block absolute left-8 w-4 bg-current animate-loading animation-delay-[-0.12s]" />
           <div className="box-border inline-block absolute left-14 w-4 bg-current animate-loading animation-delay-[0s]" />
         </div>
-      )}
-
-      {!isVideoPlaying && (
-        <div
-          className="absolute top-16 left-20 w-28 h-28 z-20"
-          style={{ backgroundColor: '#0f2425' }}
-        />
-      )}
+      )} */}
 
       {isVideoPlaying && (
         <video
-          className="absolute w-[110px] h-[110px] top-[69px] left-[84px] z-10"
+          className="absolute w-[110px] h-[110px] top-[69px] left-[84px] z-30"
           ref={videoRef}
           preload="none"
+          onPlay={() => setVideoStarted(true)}
           onEnded={handleVideoEnd}
           playsInline
         >
@@ -97,6 +92,11 @@ export function Assets({
           Your browser can&apos;t show the digimon evolution.
         </video>
       )}
+
+      <div
+        className="absolute top-16 left-20 w-28 h-28 z-20"
+        style={{ backgroundColor: '#0f2425' }}
+      />
     </>
   )
 }
